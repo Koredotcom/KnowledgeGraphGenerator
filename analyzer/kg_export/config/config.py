4d6cc3ce8bc2d5a714c155c44753a6918d077af7
satyaad-kore <satyaaditya.baratam@kore.com>
2020-03-31 16:54:22 +0530

    removed unwanted variables in config

--- analyzer/kg_export/config/config.py
+++ analyzer/kg_export/config/config.py
@@ -4,14 +4,6 @@ ontology_analyzer = {
     "PATH_COVERAGE": 50
 }
 
-log = {
-    "FORMAT_STRING": "[%(asctime)s] p%(process)s %(levelname)s - %(message)s {%(pathname)s:%(lineno)d}",
-    "SERVER_FORMAT_STRING": "[%(asctime)s] %(message)s",
-    "ONTOLOGY_ANALYZER_LOG": "log/auto_kg.log",
-    "DEBUG_LOG_LEVEL": "ERROR",
-    "SERVER_LOG_LEVEL": "INFO"
-}
-
 """ All the delimiters used by json format """
 TRAIT_DELIMITER = ':'
 SYNONYM_DELIMITER = '/'
